12d4557f9163b79d690dcd1024c0e0a106a0f7c5
Kamil Braun <kbraun@scylladb.com>
2019-08-13 18:57:24 +0200

    REFACTOR: idx_of_field
    idx_of_field size_t
    
    selectable remove todo, refactor

--- types/user.hh
+++ types/user.hh
@@ -51,6 +51,7 @@ public:
     bytes_view field_name(size_t i) const { return _field_names[i]; }
     sstring field_name_as_string(size_t i) const { return _string_field_names[i]; }
     const std::vector<bytes>& field_names() const { return _field_names; }
+    std::optional<size_t> idx_of_field(const bytes& name) const;
     sstring get_name_as_string() const;
 
 private:

3791d75173f0e46cc6d594195e8fadc7e71e3870
Piotr Jastrzebski <piotr@scylladb.com>
2019-09-04 12:32:40 +0200

    test: check that alter table with cdc manages log and desc
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- tests/cdc_test.cc
+++ tests/cdc_test.cc
@@ -29,6 +29,22 @@ SEASTAR_THREAD_TEST_CASE(test_with_cdc_parameter) {
     do_with_cql_env_thread([](cql_test_env& e) {
         auto assert_cdc = [&] (bool set) {
             BOOST_REQUIRE_EQUAL(set, e.local_db().find_schema("ks", "tbl")->cdc_enabled());
+            if (set) {
+                e.require_table_exists("ks", cdc::log_name("tbl")).get();
+                e.require_table_exists("ks", cdc::desc_name("tbl")).get();
+                auto msg = e.execute_cql(format("select node_ip, shard_id from ks.{};", cdc::desc_name("tbl"))).get0();
+                std::vector<std::vector<bytes_opt>> expected_rows;
+                expected_rows.reserve(smp::count);
+                auto ip = inet_addr_type->decompose(
+                        utils::fb_utilities::get_broadcast_address().addr());
+                for (int i = 0; i < static_cast<int>(smp::count); ++i) {
+                    expected_rows.push_back({ip, int32_type->decompose(i)});
+                }
+                assert_that(msg).is_rows().with_rows_ignore_order(std::move(expected_rows));
+            } else {
+                e.require_table_does_not_exist("ks", cdc::log_name("tbl")).get();
+                e.require_table_does_not_exist("ks", cdc::desc_name("tbl")).get();
+            }
         };
         auto alter_table_and_assert = [&] (bool cdc) {
             e.execute_cql(format("ALTER TABLE ks.tbl WITH cdc = '{}'", cdc)).get();
@@ -41,19 +57,6 @@ SEASTAR_THREAD_TEST_CASE(test_with_cdc_parameter) {
         auto test = [&] (const sstring& create_stmt, bool expected_cdc_value) {
             e.execute_cql(create_stmt).get();
             assert_cdc(expected_cdc_value);
-            if (expected_cdc_value) {
-                e.require_table_exists("ks", cdc::log_name("tbl")).get();
-                e.require_table_exists("ks", cdc::desc_name("tbl")).get();
-                auto msg = e.execute_cql(format("select node_ip, shard_id from ks.{};", cdc::desc_name("tbl"))).get0();
-                std::vector<std::vector<bytes_opt>> expected_rows;
-                expected_rows.reserve(smp::count);
-                auto ip = inet_addr_type->decompose(
-                        utils::fb_utilities::get_broadcast_address().addr());
-                for (int i = 0; i < static_cast<int>(smp::count); ++i) {
-                    expected_rows.push_back({ip, int32_type->decompose(i)});
-                }
-                assert_that(msg).is_rows().with_rows_ignore_order(std::move(expected_rows));
-            }
             alter_table_and_assert(!expected_cdc_value);
             alter_table_and_assert(expected_cdc_value);
             e.execute_cql("DROP TABLE ks.tbl").get();

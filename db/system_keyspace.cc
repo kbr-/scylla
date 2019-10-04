073be1a2d311cae98230264081190757a689ffa0
Kamil Braun <kbraun@scylladb.com>
2019-10-04 16:06:22 +0200

    db: fix/add documentation for system_keyspace

--- db/system_keyspace.cc
+++ db/system_keyspace.cc
@@ -1474,9 +1474,6 @@ std::unordered_set<dht::token> decode_tokens(set_type_impl::native_type& tokens)
     return tset;
 }
 
-/**
- * Record tokens being used by another node
- */
 future<> update_tokens(gms::inet_address ep, const std::unordered_set<dht::token>& tokens)
 {
     if (ep == utils::fb_utilities::get_broadcast_address()) {
@@ -1650,9 +1647,6 @@ future<> remove_endpoint(gms::inet_address ep) {
     });
 }
 
-    /**
-     * This method is used to update the System Keyspace with the new tokens for this node
-    */
 future<> update_tokens(const std::unordered_set<dht::token>& tokens) {
     if (tokens.empty()) {
         throw std::invalid_argument("remove_endpoint should be used instead");

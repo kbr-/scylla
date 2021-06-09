scylladb/scylladb

commit 01b6a2eb383af1bd0ea888211ca9f6120cf8d7ee

raft: randomized_nemesis_test: tick virtual clock less aggressively
Currently each tick of the virtual clock immediately schedules the next one
at the end of the task queue, but this is too aggressive. If a tick
generates work that need two tasks to be scheduled one after another
such implementation will make the task queue grow to infinity. Considering
that in the debug mode even ready future causes preemption and task
queue shuffling may cause two or more ticks to be executed without any
other work done in the middle it is very easy to get to such situation.

The patch changes the virtual clock to tick only when a shard is idle.
Message-Id: <20210606140305.2930189-1-gleb@scylladb.com>

test/raft/randomized_nemesis_test.cc
@@ -19,6 +19,7 @@
  * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <seastar/core/reactor.hh>
 #include <seastar/testing/test_case.hh>
 #include <seastar/core/timed_out_error.hh>
 #include <seastar/core/coroutine.hh>
@@ -1189,6 +1190,7 @@ public:
 class ticker {
     bool _stop = false;
     std::optional<future<>> _ticker;
+    std::optional<promise<>> _promise;
 
 public:
     ticker() = default;
@@ -1202,12 +1204,19 @@ public:
     void start(noncopyable_function<void()> on_tick, uint64_t limit = std::numeric_limits<uint64_t>::max()) {
         assert(!_ticker);
         _ticker = tick(std::move(on_tick), limit);
+        engine().set_idle_cpu_handler([this] (reactor::work_waiting_on_reactor check_for_work) {
+            if (_promise) {
+                std::exchange(_promise, std::nullopt)->set_value();
+            }
+            return idle_cpu_handler_result::no_more_work;
+        });
     }
 
     future<> abort() {
         if (_ticker) {
             _stop = true;
             co_await *std::exchange(_ticker, std::nullopt);
+            engine().set_idle_cpu_handler([] (work_waiting_on_reactor) {return idle_cpu_handler_result::no_more_work;});
         }
     }
 
@@ -1219,7 +1228,9 @@ private:
                 co_return;
             }
             on_tick();
-            co_await seastar::later();
+            assert(!bool(_promise));
+            _promise = promise<>();
+            co_await _promise->get_future();
         }
 
         tlogger.error("ticker: limit reached");
@@ -1316,7 +1327,7 @@ SEASTAR_TEST_CASE(basic_test) {
                 if (env.get_server(leader_id).is_leader()) {
                     co_return;
                 }
-                co_await seastar::later();
+                co_await timer.sleep(1_t);
             }
         })());
 

kbr-/scylla

commit 86a78e0145c9149bbcbf18ccde88115b1f210df5

raft: randomized_nemesis_test: generalize `ticker` to take a set of functions
... with associated calling periods and use the new API in `basic_test`.

Previously `ticker` would use a single function, `on_tick`, which it
called in a loop with yields in-between. In `basic_test` we would use
this to tick every object in synchrony.

However, to closely simulate a production environment, we may want the
tick ratios to be different. For example Raft servers should be ticked
rarely compared to the network.

We may also want to give the Seastar reactor more space between the
function calls (e.g. if they cause a bunch of work to be created for the
reactor that needs more than one tick to complete).

To support these use cases we generalize `ticker` to take a set of
functions with associated numbers. These numbers are the call periods of
their corresponding functions: given {n, f}, `f` will be called each
`n`th tick.

We also modify `basic_test` to use this new approach: we tick Raft
servers once per 10 network ticks (in particular, once per 10 reactor
yields).

This commit effectively reverts 01b6a2eb383af1bd0ea888211ca9f6120cf8d7ee
which caused the ticker to call `on_tick` only when the Seastar reactor
had no work to do. This approach is unfortunately incompatible with the
approach taken there. We *do* want the ticker to race with other work,
potentially producing more work while already scheduled work is executing,
and we want to see in tests what happens when we adjust the ticking ratios
of different subsystems.

The previous approach also had a problem where if there was an infinite task
loop executing, the ticker wouldn't ever tick.

The previous fix was introduced since the ticker caused too much work to
be produced (so the reactor couldn't keep up) due to ticking the Raft
servers too often (after each yield). This commit deals with the problem
in a different way, by ticking the servers rarely, which also resembles
"real-life" scenarios better.

test/raft/randomized_nemesis_test.cc
@@ -1187,10 +1187,23 @@ public:
     }
 };
 
-// Calls the given function (``tick''s) as fast as the Seastar reactor allows and yields between each call.
-// May be provided a limit for the number of calls; crashes if the limit is reached before the ticker
-// is `abort()`ed.
-// Call `start()` to start the ticking.
+// Given a set of functions and associated positive natural numbers,
+// calls the functions with periods defined by their corresponding numbers,
+// yielding in between.
+//
+// Define a "tick" to be a (possibly empty) set of calls to some of the given functions
+// followed by a yield. The ticker executes a sequence of ticks. Given {n, f}, where n
+// is a number and f is a function, f will be called each nth tick.
+//
+// For example, suppose the ticker was started with the set {{2, f}, {4, g}, {4, h}}.
+// Then the functions called in each tick are:
+// tick 1: f, g, h tick 2: none, tick 3: f, tick 4: none, tick 5: f, g, h tick 2: none, and so on.
+//
+// The order of calls within a single tick is unspecified.
+//
+// The number of ticks can be limited. We crash if the ticker reaches the limit before it's `abort()`ed.
+//
+// Call `start` to provide the distribution and start the ticking.
 class ticker {
     bool _stop = false;
     std::optional<future<>> _ticker;
@@ -1205,36 +1218,40 @@ public:
         assert(!_ticker);
     }
 
-    void start(noncopyable_function<void()> on_tick, uint64_t limit = std::numeric_limits<uint64_t>::max()) {
+    using on_tick_t = noncopyable_function<void()>;
+
+    template <size_t N>
+    void start(std::pair<size_t, on_tick_t> (&&tick_funs)[N], uint64_t limit = std::numeric_limits<uint64_t>::max()) {
         assert(!_ticker);
-        _ticker = tick(std::move(on_tick), limit);
-        engine().set_idle_cpu_handler([this] (reactor::work_waiting_on_reactor check_for_work) {
-            if (_promise) {
-                std::exchange(_promise, std::nullopt)->set_value();
-            }
-            return idle_cpu_handler_result::no_more_work;
-        });
+        _ticker = tick(std::move(tick_funs), limit);
     }
 
     future<> abort() {
         if (_ticker) {
             _stop = true;
             co_await *std::exchange(_ticker, std::nullopt);
-            engine().set_idle_cpu_handler([] (work_waiting_on_reactor) {return idle_cpu_handler_result::no_more_work;});
         }
     }
 
 private:
-    future<> tick(noncopyable_function<void()> on_tick, uint64_t limit) {
-        for (uint64_t i = 0; i < limit; ++i) {
+    template <size_t N>
+    future<> tick(std::pair<size_t, on_tick_t> (&&tick_funs)[N], uint64_t limit) {
+        static_assert(N > 0);
+
+        auto funs = std::to_array(std::move(tick_funs));
+        for (uint64_t tick = 0; tick < limit; ++tick) {
             if (_stop) {
-                tlogger.debug("ticker: finishing after {} ticks", i);
+                tlogger.debug("ticker: finishing after {} ticks", tick);
                 co_return;
             }
-            on_tick();
-            assert(!bool(_promise));
-            _promise = promise<>();
-            co_await _promise->get_future();
+
+            for (auto& [n, f] : funs) {
+                if (tick % n == 0) {
+                    f();
+                }
+            }
+
+            co_await seastar::later();
         }
 
         tlogger.error("ticker: limit reached");
@@ -1318,21 +1335,25 @@ SEASTAR_TEST_CASE(basic_test) {
     co_await with_env_and_ticker<ExReg>([&timer] (environment<ExReg>& env, ticker& t) -> future<> {
         using output_t = typename ExReg::output_t;
 
-        t.start([&] {
-            env.tick_network();
-            env.tick_servers();
-            timer.tick();
+        t.start({
+            {1, [&] {
+                env.tick_network();
+                timer.tick();
+            }},
+            {10, [&] {
+                env.tick_servers();
+            }}
         }, 10'000);
 
         auto leader_id = co_await env.new_server(true);
 
-        // Wait at most 100 ticks for the server to elect itself as a leader.
-        co_await timer.with_timeout(timer.now() + 100_t, ([] (weak_ptr<environment<ExReg>> env, raft::server_id leader_id) -> future<> {
+        // Wait at most 1000 ticks for the server to elect itself as a leader.
+        co_await timer.with_timeout(timer.now() + 1000_t, ([] (weak_ptr<environment<ExReg>> env, raft::server_id leader_id) -> future<> {
             while (true) {
                 if (!env || env->get_server(leader_id).is_leader()) {
                     co_return;
                 }
-                co_await timer.sleep(1_t);
+                co_await seastar::later();
             }
         })(env.weak_from_this(), leader_id));
 
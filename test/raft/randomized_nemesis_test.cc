0e5acf985ab0896bba46c3f113d0bbc675532e72
Kamil Braun <kbraun@scylladb.com>
2021-07-05 15:42:07 +0200

    test: raft: randomized_nemesis_test put `variant` and `monostate` `ostream` `operator<<`s into `std` namespace
    As a preparation for the next commit.
    Otherwise the definitions wouldn't be found during argument-dependent lookup
    (I don't understand why it worked before but won't after the next commit).

--- test/raft/randomized_nemesis_test.cc
+++ test/raft/randomized_nemesis_test.cc
@@ -1571,6 +1571,8 @@ struct reconfiguration {
     }
 };
 
+namespace std {
+
 std::ostream& operator<<(std::ostream& os, const std::monostate&) {
     return os << "";
 }
@@ -1580,6 +1582,8 @@ std::ostream& operator<<(std::ostream& os, const std::variant<T, Ts...>& v) {
     std::visit([&os] (auto& arg) { os << arg; }, v);
     return os;
 }
+
+} // namespace std
 
 namespace operation {
 
